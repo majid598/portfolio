c73d4c73410da8b476c8c60892b565fe9f89ada9
majid598 <143610202+majid598@users.noreply.github.com>
2024-04-30 22:58:06 +0630

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
           email: "",
           subject: "",
           message: "",
-        })
+        });
       })
       .catch((err) => toast.error(err?.data?.message));
   };
@@ -64,7 +64,10 @@ const Home = () => {
               <p>
                 Web <strong>Developer</strong> and an Enthusiastic Coder
               </p>
-              <a onClick={()=>smoothScrollTo("work")} className="button large scrolly">
+              <a
+                onClick={() => smoothScrollTo("work")}
+                className="button large scrolly"
+              >
                 Learn about what I do
               </a>
             </div>
@@ -112,7 +115,10 @@ const Home = () => {
           </div>
           <footer>
             <p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
-            <a onClick={()=>smoothScrollTo("portfolio")} className="button large scrolly">
+            <a
+              onClick={() => smoothScrollTo("portfolio")}
+              className="button large scrolly"
+            >
               See some of my recent work
             </a>
           </footer>
@@ -131,66 +137,136 @@ const Home = () => {
           <div className="row">
             <div className="col-4 col-6-medium col-12-small">
               <article className="box style2">
-                <a href="#" className="image featured">
-                  <img src="/assets/images/pic01.jpg" alt="" />
+                <a
+                  href="https://fashionista-mocha.vercel.app"
+                  target="_blank"
+                  className="image featured"
+                >
+                  <img
+                    src="/assets/images/e.png"
+                    className="w-full h-72"
+                    alt=""
+                  />
                 </a>
                 <h3>
-                  <a href="#">Magna feugiat</a>
+                  <a
+                    target="_blank"
+                    href="https://fashionista-mocha.vercel.app"
+                  >
+                    E-commerce Website
+                  </a>
                 </h3>
-                <p>Ornare nulla proin odio consequat.</p>
+                <p className="leading-5 tracking-tighter">
+                  An immersive MERN stack e-commerce website showcasing products
+                  and providing seamless shopping experiences.
+                </p>
               </article>
             </div>
             <div className="col-4 col-6-medium col-12-small">
               <article className="box style2">
-                <a href="#" className="image featured">
-                  <img src="/assets/images/pic02.jpg" alt="" />
+                <a
+                  target="_blank"
+                  href="https://github.com/majid598/Instagram-Clone"
+                  className="image featured"
+                >
+                  <img
+                    src="/assets/images/insta.jpg"
+                    className="w-full h-72"
+                    alt=""
+                  />
                 </a>
                 <h3>
-                  <a href="#">Veroeros primis</a>
+                  <a
+                    target="_blank"
+                    href="https://github.com/majid598/Instagram-Clone"
+                  >
+                    Instagram-Clone
+                  </a>
                 </h3>
-                <p>Ornare nulla proin odio consequat.</p>
+                <p className="leading-5 tracking-tighter">
+                  A feature-rich Instagram clone offering social networking
+                  functionalities for sharing photos and connecting with
+                  friends.
+                </p>
               </article>
             </div>
             <div className="col-4 col-6-medium col-12-small">
               <article className="box style2">
-                <a href="#" className="image featured">
-                  <img src="/assets/images/pic03.jpg" alt="" />
+                <a
+                  target="_blank"
+                  href="https://github.com/majid598/Spotify-Clone"
+                  className="image featured"
+                >
+                  <img
+                    src="/assets/images/spotify.jpg"
+                    className="w-full h-72"
+                    alt=""
+                  />
                 </a>
                 <h3>
-                  <a href="#">Lorem ipsum</a>
+                  <a
+                    target="_blank"
+                    href="https://github.com/majid598/Spotify-Clone"
+                  >
+                    Spotify-Clone
+                  </a>
                 </h3>
-                <p>Ornare nulla proin odio consequat.</p>
+                <p className="leading-5 tracking-tighter">
+                  A MERN stack Spotify clone delivering personalized music
+                  streaming experiences with a vast library of songs and
+                  playlists.
+                </p>
               </article>
             </div>
             <div className="col-4 col-6-medium col-12-small">
               <article className="box style2">
-                <a href="#" className="image featured">
-                  <img src="/assets/images/pic04.jpg" alt="" />
+                <a target="_blank" href="#" className="image featured">
+                  <img
+                    src="/assets/images/net.jpg"
+                    className="w-full h-72"
+                    alt=""
+                  />
                 </a>
                 <h3>
-                  <a href="#">Tempus dolore</a>
+                  <a target="_blank" href="#">
+                    Netflix-Clone
+                  </a>
                 </h3>
-                <p>Ornare nulla proin odio consequat.</p>
+                <p className="leading-5 tracking-tighter">
+                  A MERN stack Netflix clone offering a vast collection of
+                  movies and TV shows for seamless streaming and entertainment.
+                </p>
               </article>
             </div>
             <div className="col-4 col-6-medium col-12-small">
               <article className="box style2">
-                <a href="#" className="image featured">
-                  <img src="/assets/images/pic05.jpg" alt="" />
+                <a target="_blank" href="#" className="image featured">
+                  <img
+                    src="/assets/images/p4.jpg"
+                    className="w-full h-72"
+                    alt=""
+                  />
                 </a>
                 <h3>
-                  <a href="#">Feugiat aliquam</a>
+                  <a target="_blank" href="#">
+                    Food-Website
+                  </a>
                 </h3>
-                <p>Ornare nulla proin odio consequat.</p>
+                <p className="leading-5 tracking-tighter">
+                  Fast food website offering interactive menu, online ordering,
+                  and delivery for convenient dining.
+                </p>
               </article>
             </div>
             <div className="col-4 col-6-medium col-12-small">
               <article className="box style2">
-                <a href="#" className="image featured">
+                <a target="_blank" href="#" className="image featured">
                   <img src="/assets/images/pic06.jpg" alt="" />
                 </a>
                 <h3>
-                  <a href="#">Sed amet ornare</a>
+                  <a target="_blank" href="#">
+                    Sed amet ornare
+                  </a>
                 </h3>
                 <p>Ornare nulla proin odio consequat.</p>
               </article>
@@ -198,7 +274,10 @@ const Home = () => {
           </div>
           <footer>
             <p>Lorem ipsum dolor sit sapien vestibulum ipsum primis?</p>
-            <a onClick={()=>smoothScrollTo("contact")} className="button large scrolly">
+            <a
+              onClick={() => smoothScrollTo("contact")}
+              className="button large scrolly"
+            >
               Get in touch with me
             </a>
           </footer>
