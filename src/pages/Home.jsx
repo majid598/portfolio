c77d8a289e7ef98876eda6cc3d4a4fe9c1b9b940
Mr raju <ma8055965@gmail.com>
2024-06-23 22:07:09 -0700

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/ReactToastify.css";
 import Loader from "../Components/Loader";
+import { projects, works } from "../data/data";
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
@@ -51,62 +52,7 @@ const Home = () => {
     }
   };
 
-  const works = [
-    {
-      image: "/assets/images/react.png",
-      name: "React",
-      desc: "Proficient in React, I specialize in building dynamic and responsive web applications. With a solid understanding of state management, component lifecycle, and JSX syntax, I leverage React's powerful capabilities to create seamless user interfaces and optimize performance. Experienced in integrating React with various libraries and frameworks, I strive to deliver efficient and scalable solutions tailored to meet diverse project requirements.",
-    },
-    {
-      image: "/assets/images/node.png",
-      name: "Node js",
-      desc: "Skilled in Node.js, I excel in developing robust and scalable server-side applications. Leveraging Node.js's event-driven architecture and non-blocking I/O model, I specialize in building high-performance APIs and backend services. With expertise in asynchronous programming and package management using npm, I ensure efficient and reliable server-side solutions tailored to meet project objectives.",
-    },
-    {
-      image: "/assets/images/next.png",
-      name: "Next js",
-      desc: "Proficient in Next.js, I specialize in building fast, server-side rendered React applications. With Next.js's powerful features such as automatic code splitting, server-side rendering, and static site generation, I deliver efficient and SEO-friendly web applications. Leveraging Next.js's dynamic routing and API routes, I create seamless user experiences and optimize performance for modern web applications.",
-    },
-  ];
 
-  const projects = [
-    {
-      image: "/assets/images/e.png",
-      title: "E-commerce Website",
-      link: "https://fashionista-mocha.vercel.app",
-      desc: "An immersive MERN stack e-commerce website showcasing products and providing seamless shopping experiences.",
-    },
-    {
-      image: "/assets/images/insta.jpg",
-      title: "Instagram-Clone",
-      link: "https://instagram-clone-eight-lovat.vercel.app/",
-      desc: "A feature-rich Instagram clone offering social networking functionalities for sharing photos and connecting with friends.",
-    },
-    {
-      image: "/assets/images/spotify.jpg",
-      title: "Spotify-Clone",
-      link: "https://github.com/majid598/Spotify-Clone",
-      desc: "A MERN stack Spotify clone delivering personalized music streaming experiences with a vast library of songs and playlists",
-    },
-    {
-      image: "/assets/images/net.jpg",
-      title: "Netflix-Clone",
-      link: "#",
-      desc: "A MERN stack Netflix clone offering a vast collection of movies and TV shows for seamless streaming and entertainment.",
-    },
-    {
-      image: "/assets/images/p4.jpg",
-      title: "Food-Website",
-      link: "https://burgerpizzapoint.vercel.app/",
-      desc: "Fast food website offering interactive menu, online ordering, and delivery for convenient dining.",
-    },
-    {
-      image: "/assets/images/sherry.png",
-      title: "Animated-Website",
-      link: "https://sherryjswebsite.netlify.app/",
-      desc: "Animated website built with Sherry.js, featuring captivating visuals and interactive elements for an immersive experience.",
-    },
-  ];
 
   return (
     <div className="relative">
